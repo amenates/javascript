e2ac9b2ff1f065d49eebaf0d7a953510c2981c4a
Julia Nikolaeva <julia.nikolaeva@myoffice.team>
2019-11-13 11:14:29 +0300

    add cycle "for"

---
+++ jsCyrcle/script.js
@@ -0,0 +1,19 @@
+function f1() {
+  let p = document.getElementById('out_f1');
+  let i;
+
+// цикл FOR:
+  //
+  for (i = 0; i < 50; i++) {
+    p.innerHTML += i + ' ';
+  }
+}
+// i++ this is i = i + 1
+//
+// function f2() {
+//
+// }
+//
+// function f3() {
+//
+// }
