b6b2d78789e61b8a597d8b4030462820975245c6
Julia Nikolaeva <julia.nikolaeva@myoffice.team>
2019-11-13 15:42:31 +0300

    add calculator

---
+++ jsCalculator/script.js
@@ -0,0 +1,49 @@
+function plus() {
+  let int1 = document.getElementById('int1').value;
+  int1 = parseInt(int1);
+  let int2 = document.getElementById('int2').value;
+  int2 = parseInt(int2);
+  let pOut = document.getElementById('out');
+
+  pOut.innerHTML = int1 + int2;
+}
+
+function minus() {
+  let int1 = document.getElementById('int1').value;
+  int1 = parseInt(int1);
+  let int2 = document.getElementById('int2').value;
+  int2 = parseInt(int2);
+  let pOut = document.getElementById('out');
+
+  pOut.innerHTML = int1 - int2;
+}
+
+function multi() {
+  let int1 = document.getElementById('int1').value;
+  int1 = parseInt(int1);
+  let int2 = document.getElementById('int2').value;
+  int2 = parseInt(int2);
+  let pOut = document.getElementById('out');
+
+  pOut.innerHTML = int1 * int2;
+}
+
+function division() {
+  let int1 = document.getElementById('int1').value;
+  int1 = parseInt(int1);
+  let int2 = document.getElementById('int2').value;
+  int2 = parseInt(int2);
+  let pOut = document.getElementById('out');
+
+  pOut.innerHTML = int1 / int2;
+}
+
+function test() {
+  let num1 = document.getElementById('num1').value;
+  num1 = parseInt(num1);
+  let num2 = document.getElementById('num2').value;
+  num2 = parseInt(num2);
+  let pOut2 = document.getElementById('out2');
+
+  pOut2.innerHTML = (num1 / 100) * num2; // вычисляет процент
+}
