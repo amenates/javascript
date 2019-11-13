5e8e2b0d3cb14714f96af5f90585de57192ba2a9
Julia Nikolaeva <julia.nikolaeva@myoffice.team>
2019-11-13 17:13:30 +0300

    fix

--- jsCyrcle/script.js
+++ jsCyrcle/script.js
@@ -2,18 +2,20 @@ function f1() {
   let p = document.getElementById('out_f1');
   let i;
 
-// цикл FOR:
-  //
-  for (i = 0; i < 50; i++) {
+// цикл FOR с заданным количеством повторений
+  for (i = 0; i < 100; i++) {
+    // i++ this is i = i + 1
+    // начальный старт цикла; условие при котором цикл выполняется; шаг цикла
     p.innerHTML += i + ' ';
   }
 }
-// i++ this is i = i + 1
-//
-// function f2() {
-//
-// }
-//
-// function f3() {
-//
-// }
+
+// цикл WHILE
+function f2() {
+  let p = document.getElementById('out_f2');
+  let j;
+}
+
+function f3() {
+
+}
