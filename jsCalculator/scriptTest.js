bb50175cfd08dd7cf977fd0a5539aaf6eebc725a
Amenates <iamenates@gmail.com>
2020-01-14 23:34:17 +0300

    ...

--- jsCalculator/scriptTest.js
+++
@@ -1,9 +0,0 @@
-let int1 = document.getElementById('int1').value;
-int1 = parseInt(int1);
-let int2 = document.getElementById('int2').value;
-int2 = parseInt(int2);
-let pOut = document.getElementById('out');
-
-function plus() {
-  pOut.innerHTML = int1 + int2;
-}
